66f46f90f8f153ca632e27c02e65034719252f31
kushali13 <kushalisanghvi1311@gmail.com>
2025-05-28 17:11:46 +0530

    Update urls.py

--- path_game/urls.py
+++ path_game/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('game.urls')),  # include URLs from the game app
+    path('', include('game.urls')),
 ]
